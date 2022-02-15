9099e4487e5ef5a2d0bde59f4b277148015ac549
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-02-15 13:28:05 -0300

    Adding server side generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { stripe } from "../services/stripe";
 
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KTQzZFRSKgoTHfEfVn8ZEsB");
 
   const product = {
@@ -55,5 +55,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
