8ba451ed431284981902fadbe70ebeda9974c9b3
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-02-15 21:54:26 -0300

    Adding faunaDB queries and config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q, query } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -13,4 +17,29 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+
+      return true;
+    },
+  },
 });
