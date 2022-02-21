1f66b1800d3570fa3f8e21662063b8cc1de9dd9b
Vinicius Inocencio de Souza <vicnicius2009@gmail.com>
2022-02-21 09:19:41 -0300

    Adding stripe events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream"
 import { Stripe } from "stripe";
 import { stripe } from "../../services/stripe"
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = []
@@ -22,7 +23,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  "checkout.session.completed"
+  "checkout.session.completed",
+  "checkout.session.created",
+  "checkout.session.updated",
+  "checkout.session.deleted"
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -41,8 +45,27 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     const { type } = event;
 
-    if (!relevantEvents.has(type)) {
-      console.log("Evento recebido", event)
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+
+            break;
+
+          default:
+            throw new Error("Unhandled event")
+        }
+      } catch (err) {
+        return res.json({ error: "Webhook handler failed" })
+      }
+
     }
 
     res.json({ ok: true })
