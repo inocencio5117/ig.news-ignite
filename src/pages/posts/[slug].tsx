13f88ec198e66116f861b1e42bf80a091b508aa4
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-03-07 16:16:34 -0300

    Adding post and styling

---
+++ src/pages/posts/[slug].tsx
@@ -0,0 +1,64 @@
+import { getSession } from "next-auth/react"
+import { Head } from "next/document";
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../services/prismic";
+
+import styles from './post.module.scss'
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    constent: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <Head>
+        <title>{post.title} | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.constent }} />
+        </article>
+      </main>
+
+    </>
+  )
+}
+
+export const getServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req });
+  const { slug } = params;
+
+  // if (!session) {}
+
+  const prismic = getPrismicClient(req)
+
+  const response = await prismic.getByUID('publication', String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post
+    }
+  }
+}
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -6,6 +6,7 @@ import { RichText } from "prismic-dom"
 
 import Head from "next/head";
 import styles from "./styles.module.scss"
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -31,11 +32,13 @@ export default function Posts({ posts }: PostsProps) {
         <div className={styles.posts}>
           {
             posts.map(post => (
-              <a key={post.slug} href="#">
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-              </a>
+              <Link href={`/posts/${post.slug}`}>
+                <a key={post.slug}>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
             ))
           }
         </div>
