598fc20c4298fd1fec8faae4306173754ff5b635
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-04 21:16:59 -0300

    Test: tests for Header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from "@testing-library/react";
+import { Header } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+jest.mock("next-auth/react", () => {
+  return {
+    useSession() {
+      return [null, false];
+    },
+  };
+});
+
+describe("Active Link Component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(<Header />);
+
+    expect(getByText("Home")).toBeInTheDocument();
+    expect(getByText("Posts")).toBeInTheDocument();
+  });
+});
