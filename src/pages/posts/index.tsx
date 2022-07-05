2bcb1fc22a78a66ccc274907c223057f8da475b8
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-05 16:55:28 -0300

    Test: adding Posts tests

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,11 @@
 import { GetStaticProps } from "next";
 import { getPrismicClient } from "../../services/prismic";
 
-import Prismic from "@prismicio/client"
-import { RichText } from "prismic-dom"
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import Head from "next/head";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
 import Link from "next/link";
 
 type Post = {
@@ -13,34 +13,30 @@ type Post = {
   title: string;
   excerpt: string;
   updatedAt: string;
-}
+};
 
 interface PostsProps {
-  posts: Post[]
+  posts: Post[];
 }
 
 export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
-        <title>
-          Posts | Ignews
-        </title>
+        <title>Posts | Ignews</title>
       </Head>
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          {
-            posts.map(post => (
-              <Link href={`/posts/${post.slug}`}>
-                <a key={post.slug}>
-                  <time>{post.updatedAt}</time>
-                  <strong>{post.title}</strong>
-                  <p>{post.excerpt}</p>
-                </a>
-              </Link>
-            ))
-          }
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
@@ -50,27 +46,33 @@ export default function Posts({ posts }: PostsProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'publication')
-  ], {
-    fetch: ['publication.title', 'publication.content'],
-    pageSize: 100
-  })
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "publication")],
+    {
+      fetch: ["publication.title", "publication.content"],
+      pageSize: 100,
+    }
+  );
 
-  const posts = response.results.map(post => {
+  const posts = response.results.map((post) => {
     return {
       slug: post.id,
       title: RichText.asText(post.data.title),
-      execrpt: post.data.content?.find(content => content.type === 'paragraph')?.text ?? '',
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
-        day: '2-digit',
-        month: 'long',
-        year: 'numeric',
-      })
-    }
-  })
+      execrpt:
+        post.data.content?.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
 
   return {
-    props: {}
-  }
-}
+    props: {},
+  };
+};
